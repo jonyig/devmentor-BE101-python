7441df2eb790fdb540d0a52a910884e372b17910
robYP <robchen12@hotmail.com>
2024-06-19 22:55:42 +0800

    create instances for Language/Visitor/Event/TG/Email/Sms create 3 events:signup/subscribe/cancel

--- event.py
+++ event.py
@@ -3,3 +3,21 @@
 class Event:
     def notify(self,visitor):
         print(f"Notify {visitor}")
+
+    def __init__(self, event_name: str):
+        if event_name == "signup":
+            print("Hello signup")
+        elif event_name == "subscribe":
+            print("hello subscribe")
+        elif event_name == "cancel":
+            print("hello cancel")
+
+    # def signup(self):
+    #     self.notify()
+    #     return
+    #
+    # def subscribe(self):
+    #     return
+    #
+    # def cancel(self):
+    #     return
--- student.py
+++ student.py
@@ -1,3 +1,4 @@
 from visitor import Visitor
+
 class Student(Visitor):
     pass
