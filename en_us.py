605c6af6f8041452aeb8dc884fb75aec418b59bd
robYP <robchen12@hotmail.com>
2024-07-05 00:12:24 +0800

    modifed class Zhtw and Enus to inherit from Language (realization), and include msg in both classed in different languages

--- en_us.py
+++ en_us.py
@@ -1,3 +1,13 @@
-class EnUs:
-    def __str__(self):
-        print("This is English - US")
+from language import Language
+
+
+class Enus(Language):
+    def __init__(self):
+        self.msg = {
+            "signup": "signed up SUCCESSFULLY",
+            "subscribe": "subscribed SUCCESSFULLY",
+            "cancel": "cancelled SUCCESSFULLY"
+        }
+
+    def get_msg(self, msg_key: str) -> str:
+        return self.msg[msg_key]
