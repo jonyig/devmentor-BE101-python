4b3b512ef43b096dcfa28e218843d77383765278
Jonny <ji394su311203106@gmail.com>
2024-09-19 19:01:53 +0800

    feat: c

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def main(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main('PyCharm123ab')
+    main('PyCharm123abc')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
