ac819d6ac77b9a443d5035215f677241d503f73c
Jimmy-web169 <jimmyj20101@gmail.com>
2024-09-24 12:52:53 +0800

    create User class

--- main.py
+++ main.py
@@ -3,6 +3,23 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+# User class
+class User:
+    def __init__(self, id: str, email: str, phone: str, prefers_language: str):
+        self.id = id
+        self.email = email
+        self.phone = phone
+        self.prefers_language = prefers_language
+
+    def register(self):
+        return RegisterEvent()
+
+    def course_booking(self):
+        return CourseBookingEvent()
+
+    def course_cancel(self):
+        return CourseCancelEvent()
+
 
 def main(name):
     # Use a breakpoint in the code line below to debug your script.
