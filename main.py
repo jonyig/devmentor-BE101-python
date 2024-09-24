a78d063d821f762e112f4f684a9d1451d85e954f
Jimmy-web169 <jimmyj20101@gmail.com>
2024-09-24 12:56:18 +0800

    create EventManager

--- main.py
+++ main.py
@@ -104,7 +104,22 @@ class CourseCancelEvent(Event):
         for channel in self.channels:
             channel.send_message(message, user)
 
+# UserEventManager manage user's event
+class UserEventManager:
+    def __init__(self, user: User):
+        self.user = user
 
+    def user_register(self):
+        register_event = self.user.register()
+        register_event.notify(self.user)
+
+    def book_course(self):
+        booking_event = self.user.course_booking()
+        booking_event.notify(self.user)
+
+    def cancel_course(self):
+        cancel_event = self.user.course_cancel()
+        cancel_event.notify(self.user)
 def main(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi123456, {name}')  # Press ⌘F8 to toggle the breakpoint.
