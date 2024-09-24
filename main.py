52eb4ada754a4957911435f8f0718220a19699b7
Jimmy-web169 <jimmyj20101@gmail.com>
2024-09-24 12:56:18 +0800

    create EventManager

--- main.py
+++ main.py
@@ -3,7 +3,22 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+# UserEventManager manage user's event
+class UserEventManager:
+    def __init__(self, user: User):
+        self.user = user
 
+    def user_register(self):
+        register_event = self.user.register()
+        register_event.notify(self.user)
+
+    def book_course(self):
+        booking_event = self.user.course_booking()
+        booking_event.notify(self.user)
+
+    def cancel_course(self):
+        cancel_event = self.user.course_cancel()
+        cancel_event.notify(self.user)
 def main(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi123456, {name}')  # Press ⌘F8 to toggle the breakpoint.
