972509ee54e9350e99e957ea8a5278e08db2fadd
Jimmy-web169 <jimmyj20101@gmail.com>
2024-09-24 19:42:38 +0800

    User class

--- main.py
+++ main.py
@@ -4,22 +4,6 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 from abc import ABC, abstractmethod
 
-# User class
-class User:
-    def __init__(self, id: str, email: str, phone: str, prefers_language: str):
-        self.id = id
-        self.email = email
-        self.phone = phone
-        self.prefers_language = prefers_language
-
-    def register(self):
-        return RegisterEvent()
-
-    def course_booking(self):
-        return CourseBookingEvent()
-
-    def course_cancel(self):
-        return CourseCancelEvent()
 
 
 # UserEventManager manage user's event
---
+++ user.py
@@ -0,0 +1,16 @@
+# User class
+class User:
+    def __init__(self, id: str, email: str, phone: str, prefers_language: str):
+        self.id = id
+        self.email = email
+        self.phone = phone
+        self.prefers_language = prefers_language
+
+    def register(self):
+        return RegisterEvent()
+
+    def course_booking(self):
+        return CourseBookingEvent()
+
+    def course_cancel(self):
+        return CourseCancelEvent()
