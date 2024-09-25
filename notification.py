dfcd5a62c7b42b2b8ace9d80069eced8eb191cbe
Jimmy-web169 <jimmyj20101@gmail.com>
2024-09-25 10:24:18 +0800

    Update Notification class, import package

--- notification.py
+++ notification.py
@@ -1,24 +1,24 @@
 from abc import ABC, abstractmethod
-
+from user import User
 # Notification Interface
 class Notification(ABC):
 
     @abstractmethod
-    def send_message(self, message: str, user: 'User'):
+    def send_message(self, message: str, user: User):
         """
         Every notification should have a send_message method
         """
         pass
 # Email Notification
 class Email(Notification):
-    def send_message(self, message: str, user: 'User'):
+    def send_message(self, message: str, user: User):
         print(f"Sending Email to {user.email}: {message}")
 # SMS Notification
 class SMS(Notification):
-    def send_message(self, message: str, user: 'User'):
+    def send_message(self, message: str, user: User):
         print(f"Sending SMS to {user.phone}: {message}")
 
 # Telegram Notification
 class Telegram(Notification):
-    def send_message(self, message: str, user: 'User'):
+    def send_message(self, message: str, user: User):
         print(f"Sending Telegram message to {user.id}: {message}")
