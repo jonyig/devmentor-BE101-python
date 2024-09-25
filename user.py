35a7e2a779879b620e54d92e4de196571a9d8931
Jimmy-web169 <jimmyj20101@gmail.com>
2024-09-25 10:23:20 +0800

    Update User class, user function return str

--- user.py
+++ user.py
@@ -1,16 +1,17 @@
 # User class
 class User:
     def __init__(self, id: str, email: str, phone: str, prefers_language: str):
+
         self.id = id
         self.email = email
         self.phone = phone
         self.prefers_language = prefers_language
 
     def register(self):
-        return RegisterEvent()
+        return "RegisterEvent"
 
     def course_booking(self):
-        return CourseBookingEvent()
+        return "CourseBookingEvent"
 
     def course_cancel(self):
-        return CourseCancelEvent()
+        return "CourseCancelEvent"
