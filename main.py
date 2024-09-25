295f9bd55c54b41cf3cf8e5581b3cb97eb549668
Jimmy-web169 <jimmyj20101@gmail.com>
2024-09-25 10:28:05 +0800

    1.import package 2.clear parameter in main function

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from user import User
+from eventmanager import UserEventManager
 
 
-
-def main(name):
+def main():
     # create user
     user = User(id="A0001", email="jimmy@example.com", phone="0912345678", prefers_language="zh-TW")
 
@@ -24,6 +25,6 @@ def main(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
